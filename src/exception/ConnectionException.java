4c0be7c421fe9f88d9549b22e3acd88fd3c48fc4
2dam <2dam@HZ375747.TartangaLH.eus>
2023-10-18 12:09:16 +0200

    version 1 de las server

---
+++ src/exception/ConnectionException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Eneko.
+ */
+public class ConnectionException extends Exception {
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+}
